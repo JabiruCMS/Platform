0663e29eca10044a44d4c6c491b0a2717848d4b1
David Cap <david.cap@bocians.net>
2022-06-20 20:39:38 -0600

    latest updates to bring all modules up to speed with L9/mysql8

--- config/laravellocalization.php
+++ config/laravellocalization.php
@@ -7,6 +7,7 @@ return [
     // Regional languages are sorted by their base languge, so "British English" sorts as "English, British"
     'supportedLocales' => [
         'en' => ['name' => 'English', 'script' => 'Latn', 'native' => 'English'],
+        'cs' => ['name' => 'Czech', 'script' => 'Latn', 'native' => 'Česky'],
     ],
     // Negotiate for the user locale using the Accept-Language header if it's not defined in the URL?
     // If false, system will take app.php locale attribute
